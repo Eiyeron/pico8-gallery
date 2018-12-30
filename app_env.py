3dfa021d40755d90d975fc4a6dd22ac25ef8548e
Eiyeron Fulmincendii <eiyeron@gmail.com>
2018-12-30 15:37:39 +0100

    Added link section

--- app_env.py
+++ app_env.py
@@ -21,31 +21,36 @@ def build_static_url(*args):
 def determine_image_url(cart, folder_name):
     url_cart_root = build_url("carts", folder_name)
     fs_cart_root = path.join(config["Path"]["cart_dir"], folder_name)
-    
+
     if "cart" in cart:
         return path.join(url_cart_root, cart["cart"])
 
     elif path.isfile(path.join(fs_cart_root, folder_name+".p8.png")):
         return path.join(url_cart_root, folder_name+".p8.png")
-    
+
     elif path.isfile(path.join(fs_cart_root, folder_name+".png")):
         return path.join(url_cart_root, folder_name+".png")
-    
+
     return build_static_url("img/missing_cart.png")
 
 
 def determine_cart_url(cart, folder_name):
     url_cart_root = build_url("carts", folder_name)
     fs_cart_root = path.join(config["Path"]["cart_dir"], folder_name)
-    
+
     if "index" in cart:
         return build_url(fs_cart_root, cart["index"])
 
     elif path.isfile(path.join(fs_cart_root, folder_name+".html")):
         return path.join(url_cart_root, folder_name+".html")
-    
+
     return "#"
 
+def get_links():
+    if config.has_section("Links"):
+        return [entry.split(',') for key,entry in config.items("Links")]
+
+    return []
 
 def load_carts(file_path):
     cart_entries = []
--- update_index.py
+++ update_index.py
@@ -1,4 +1,4 @@
-from app_env import config, env, determine_image_url, determine_cart_url, load_carts
+from app_env import config, env, determine_image_url, determine_cart_url, load_carts, get_links
 from os import path
 import json
 
@@ -7,6 +7,6 @@ def update_index():
     index_template = env.get_template('index.j2')
     carts = load_carts(path.join(config["Path"]["cart_dir"], 'carts.json'))
     print("Found {} carts.".format(len(carts)))
-    index_result = index_template.render(carts=carts)
+    index_result = index_template.render(carts=carts, links=get_links())
     with open("dist/index.html", "w") as output:
         output.write(index_result)
